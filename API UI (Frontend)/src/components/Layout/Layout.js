8e4685aa010e6e67effd28a76bb78cbe1a0a3ac7
Shreyas Sreenivas <shreyassreenivas324@gmail.com>
2025-05-04 00:35:51 +0000

    Some UI changes

--- API UI (Frontend)/src/components/Layout/Layout.js
+++ API UI (Frontend)/src/components/Layout/Layout.js
@@ -4,23 +4,16 @@ import { Container } from 'react-bootstrap';
 import Header from './Header';
 import Footer from './Footer';
 import { useLocation } from 'react-router-dom';
-import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   const location = useLocation();
-  
-  // Determine if we should show sidebar based on route
-  // Added '/docs' route to the list
-  const showSidebar = location.pathname.startsWith('/dashboard') ||
-                     location.pathname.startsWith('/docs');
   
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
       
       <div className="flex-grow-1 d-flex">
-        {showSidebar && <Sidebar />}
-        
+      
         <main className="flex-grow-1 bg-light">
           <Container fluid className="py-4">
             {children}
